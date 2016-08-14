eb9d9679839658efa0ada4f6c3903341e43b7fa3
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-13 20:37:36 -0700

    helloworld.php random background color is implemented

--- helloworld.php
+++ helloworld.php
@@ -2,7 +2,13 @@
  <head>
   <title>PHP Test</title>
  </head>
- <body>
- <?php echo '<p>Hello Web World from PHP on' . date("Y/m/d") . date("h:i:sa") . '</p>'; ?> 
+ <?php
+  $bg = array('red', 'blue', 'white'); // array of filenames
+
+  $i = rand(0, count($bg)-1); // generate random number size of the array
+  $selectedBg = "$bg[$i]"; // set variable equal to which random color was chosen
+?>
+ <body style="background:<?php echo $selectedBg; ?>">
+ <?php echo '<p>Hello Web World from PHP on' . date("Y/m/d") .' '. date("h:i:sa") . '</p>'; ?> 
  </body>
 </html>
