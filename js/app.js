147e3c1de197c86184e94de1ecf81b73a22f3686
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-16 10:40:15 -0700

    Node JS dynamic file 3rd try

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 /**
  * Created by Hirad on 8/14/2016.
  */
-var http = require('http');
+//var http = require('http');
 var express = require('express');
 var app = express();
 
 app.get("/node", function (req,res) {
     res.render("../home.ejs");
 });
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    //res.end('Hello Apache!\n');
-    
-}).listen(8000, '198.199.116.102');
+app.listen(8000,'198.199.116.102',function () {
+    console.log("Server is running");
+});
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     //res.end('Hello Apache!\n');
+//
+// }).listen(8000, '198.199.116.102');
