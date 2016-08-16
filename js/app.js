918fd6c0bec30f321d0d1fcf69f266b9a038d997
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-16 10:26:17 -0700

    Node JS dynamic file

--- js/app.js
+++ js/app.js
@@ -4,5 +4,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.send('Hello Apache!\n');
+    //res.end('Hello Apache!\n');
+    res.render("../home.ejs");
 }).listen(8000, '198.199.116.102');
