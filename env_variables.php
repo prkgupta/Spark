3a19d3f2e7ac4b28028489e22182b018b70f1573
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-16 11:19:51 -0700

    Environment Variables in PHP

---
+++ env_variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Environment Variables in PHP</title>
+</head>
+<body >
+
+<h1>Printing all the environment variables from PHP</h1>
+
+<?php var_dump($_ENV) ?>
+
+</body>
+</html>
+
