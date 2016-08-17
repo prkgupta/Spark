8ad11c150cf57955abd2d8fc8ee54b241f3d7a68
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-16 18:30:03 -0700

    html update

--- js/main.js
+++ js/main.js
@@ -10,7 +10,21 @@ function changeSubmit(){
             form.action="http://198.199.116.102:8000/form";
             break;
         default:
-            form.action="color.pl";
+            form.action="color.php";
     }
 }
 
+var sessionForm = document.getElementById("sessionForm");
+
+function changeSubmitForSession(){
+    switch (lang.value) {
+        case "2":
+            sessionForm.action="cgi-bin/session.pl";
+            break;
+        case "3":
+            sessionForm.action="http://198.199.116.102:8000/sessionForm";
+            break;
+        default:
+            sessionForm.action="session.php";
+    }
+}
