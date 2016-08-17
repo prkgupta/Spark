a729082f00b3a3bff30e06dd9d0ff005c48265cf
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-16 17:03:35 -0700

    perl part 1

--- cgi-bin/helloworld.pl
+++ cgi-bin/helloworld.pl
@@ -12,8 +12,10 @@ print "<title>Hello world in Perl!</title>";
 print "</head>\n";
 @colors = ("blue", "white", "red");
 $num = int rand(3);
+print $num
 #print "<body style=\"background-color:$colors[$num]\"">\n";
 print "<body>\n";
+#print "<h1> $colors[$num] </h1>"
 $datestring = localtime();
 print "<h1>Hello, world from $datestring!</h1>\n";
 print "</body> </html>\n";
