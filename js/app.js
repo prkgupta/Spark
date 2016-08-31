6932d412d2bbc8d63ada6531b1762ef3c4b56203
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-08-30 21:18:51 -0700

    Testing mysql fuctionality

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var mysql       = require('mysql');
 var connection  = mysql.createConnection({
     host     : 'localhost',
     port     : 3306,
-    user     : 'root',
+    user     : 'user',
     password : 'pass',
     database : 'movies'
 });
