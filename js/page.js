3081b34dd793c6647224c2fe3af73184bec72489
Prakriti Gupta <tian@Prakritis-MacBook-Pro.local>
2016-08-31 22:29:35 -0700

    Optimization JS

---
+++ js/page.js
@@ -0,0 +1,13 @@
+/**
+ * Created by  on 8/31/2016.
+ */
+
+//table page limit
+$(document).ready(function() {
+        $('#tableData').paging({limit:5});
+});  
+
+//table sort
+$(document).ready(function() { 
+        $('#tableData').tablesorter(); 
+});    
