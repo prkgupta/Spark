c0355ea4ed2d55f5d4021e71b0eab788968c67e7
Hirad Khakipour <hiradkhakipour@hotmail.com>
2016-09-01 00:04:27 -0700

    Paging updates

--- js/page.js
+++ js/page.js
@@ -4,15 +4,8 @@
 
 //table page limit
 $(document).ready(function() {
-    //$('#tableData').paging({limit:5});
+    $('#tableData').paging({limit:5});
 });
-
-function page(){
-    var pageLimit = $('.itemsPerPage').value;
-    pageLimit = pageLimit == 'all'? 1000000 : pageLimit;
-    console.log("limit is " + pageLimit);
-    $('#tableData').paging({limit:pageLimit});
-}
 
 //table sort
 $(document).ready(function() { 
